d675e065fa4dbb2554471a5db453a94d9a96f66d
Alexander Echeverry <alexanderjr.er@gmail.com>
2021-08-20 10:39:21 -0500

    entrega

---
+++ Semana 1/primer-seguimiento/src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import control.FormWindow;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FormWindow window = new FormWindow();
+        window.build().show();
+    }
+}
